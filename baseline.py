9a4df3dffdcf334cbb9e509abc4bd148e0817f61
lester-lee <19ll6@lohani.cs.williams.edu>
2017-10-29 23:23:58 -0400

    processed documents into tokens

--- baseline.py
+++ baseline.py
@@ -1,62 +1,58 @@
 #Lester Lee & Vincent Lin
-import string
+import string, re
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 
-#question processing: remove stop words from question
-
 stopword_list = stopwords.words('english')
+def preprocess(s, getstopwords=False):
+        #given s, tokenize and remove punctuation
+        s = s.lower()
+        s = word_tokenize(s)
+        s = [w.translate(None, string.punctuation) for w in s]
+        s = list(filter(None, s))
+        if getstopwords:
+                return ([w for w in s if not w in stopword_list],
+                        [w for w in s if w in stopword_list])
+        else:
+                return [w for w in s if not w in stopword_list]
 
+#question processing: remove stop words from question
 with open("qadata/train/questions.txt") as question_file:
 	question_list = list(filter(None,question_file.read().split("\n")))
 	questions = {}
 	for i in range(0, len(question_list), 2):
 		#Lower case everything, tokenize, then remove punctuation, and then remove stopwords
 		qnum = question_list[i].split()[-1]
-		question = question_list[i+1].lower() 
-		question = word_tokenize(question)
-		question = [w.translate(None, string.punctuation) for w in question]
-		question = list(filter(None, question))
-		questions[qnum] =  ([w for w in question if not w in stopword_list], [w for w in question if w in stopword_list])
+		question = preprocess(question_list[i+1], getstopwords=True)
+		questions[qnum] = question
 #questions is a dictionary {question#: question tokens}
 		
 #passage retrieval
 for qnum in questions:
 	docname = "top_docs.{}".format(qnum)
+        textRE = re.compile(r'<TEXT>(.*)</TEXT>', re.DOTALL)
 	with open("topdocs/train/{}".format(docname)) as docfile:
-		#parse docfile into etree
+		#for each qid, make a string containing everything within <text> tags
 		qidlist = []
 		textlist = []
+                parsedtextlist = []
 		textstring= ""
-		oneTextfile = False
 		for docstring in docfile:
-			
-			#get the qid
-			if docstring.startswith("Qid:"):
-				qidlist.append(docstring)
-				textlist.append(textstring)
-				textstring = ""
-			elif oneTextfile:
-				textstring+=docstring
+                        if docstring.startswith("Qid:"):
+                                qidlist.append(docstring)
+                                textlist.append(textstring)
+                                textstring = ""
+                        else:
+                                textstring += docstring
 
-			#Get all elements that starts with the <text> and end with </text>
-			#since there may be multiple elements in one qid
-			if docstring.startswith("<TEXT>"):
-				oneTextfile = True
-			if docstring.startswith("</TEXT>"):
-				oneTextfile = False
-		#print textlist
-		print len(textlist)
-		#print qidlist
-		print len(qidlist)
-		#tuples of qidlist and text
-		qidtextTuple = zip(qidlist, textlist)
-		print qidtextTuple
+                for textchunk in textlist:
+                        texts = textRE.findall(textchunk)
+                        tempstring = " ".join(texts)
+                        parsedtextlist.append(preprocess(tempstring))
 
-
-
-
-
+		qidtextTuple = zip(qidlist, parsedtextlist)
+		print(qidtextTuple)
+                
 		#retrieve appropriate 10-grams
 		#map each 10-gram to binary feature vector with question as bag of words
 		#compute vector similarity score between question & each feature vector
