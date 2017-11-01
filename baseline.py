d2e6cecc85d344e81af4baf2007aa215a016d824
adroitous <32344162+adroitous@users.noreply.github.com>
2017-11-01 11:58:23 -0400

    added the vector methods still need <p> and bugfix

--- baseline.py
+++ baseline.py
@@ -2,11 +2,6 @@
 import string, re
 from nltk.corpus import stopwords
 from nltk import word_tokenize
-<<<<<<< HEAD
-
-#question processing: remove stop words from question
-=======
->>>>>>> 4ff006b4ce057419f5cf9b9f456a2ed6a55ec637
 
 stopword_list = stopwords.words('english')
 def preprocess(s, getstopwords=False):
@@ -21,11 +16,9 @@ def preprocess(s, getstopwords=False):
         else:
                 return [w for w in s if not w in stopword_list]
 
-<<<<<<< HEAD
-=======
 #find the ngram 
 # the number = n
-def ngram(textlist, number):
+def ngram(textlist, number, questions):
 	for text in textlist:
 		ngramlist =[]
 		for n in range(len(text)-number+1):
@@ -33,75 +26,40 @@ def ngram(textlist, number):
 			for e in range(number):
 				singleNgram.append(text[e+n])
 			ngramlist.append(singleNgram)
+			compare_ngram(ngramlist, questions)
 	return ngramlist
 
-'''
-questions about vector similarity and etc require more thoughts
-def compare_ngram(text, wordbag):
-	for e in text:
-		if e is in wordbag:
-			count+=1
-'''
+#since the vector are [1 1 1 1] *[ x y z w] where x,y,z,w are the frequency of each word from the wordbag, 
+#we can compute this by summing all the x,y,z,w together. 
+#the length of the vector is determined by the wordbag.
+def compare_ngram(ngramlist, wordbag):
+	frequency = []
+	for e in ngramlist:
+		# e is a list of tokens
+		#
+		#help pls
+		#i want to use the findall expression.
+		# is it possible to use the findall regular expression on it?
+		#
+		frequency.append(sum(len(e.findall(y for y in wordbag)), e))
+	return frequency
+
 #question processing: remove stop words from question
->>>>>>> 4ff006b4ce057419f5cf9b9f456a2ed6a55ec637
 with open("qadata/train/questions.txt") as question_file:
 	question_list = list(filter(None,question_file.read().split("\n")))
 	questions = {}
 	for i in range(0, len(question_list), 2):
 		#Lower case everything, tokenize, then remove punctuation, and then remove stopwords
 		qnum = question_list[i].split()[-1]
-<<<<<<< HEAD
-		question = question_list[i+1].lower() 
-		question = word_tokenize(question)
-		question = [w.translate(None, string.punctuation) for w in question]
-		question = list(filter(None, question))
-		questions[qnum] =  ([w for w in question if not w in stopword_list], [w for w in question if w in stopword_list])
-=======
 		question = preprocess(question_list[i+1], getstopwords=True)
 		questions[qnum] = question
 		#print questions
->>>>>>> 4ff006b4ce057419f5cf9b9f456a2ed6a55ec637
+		#print questions
 #questions is a dictionary {question#: question tokens}
 		
 #passage retrieval
 for qnum in questions:
 	docname = "top_docs.{}".format(qnum)
-<<<<<<< HEAD
-	with open("topdocs/train/{}".format(docname)) as docfile:
-		#parse docfile into etree
-		qidlist = []
-		textlist = []
-		textstring= ""
-		oneTextfile = False
-		for docstring in docfile:
-			
-			#get the qid
-			if docstring.startswith("Qid:"):
-				qidlist.append(docstring)
-				textlist.append(textstring)
-				textstring = ""
-			elif oneTextfile:
-				textstring+=docstring
-
-			#Get all elements that starts with the <text> and end with </text>
-			#since there may be multiple elements in one qid
-			if docstring.startswith("<TEXT>"):
-				oneTextfile = True
-			if docstring.startswith("</TEXT>"):
-				oneTextfile = False
-		#print textlist
-		print len(textlist)
-		#print qidlist
-		print len(qidlist)
-
-		qidtextTuple = zip(qidlist, textlist)
-			print qidtextTuple
-
-
-
-
-
-=======
         textRE = re.compile(r'<TEXT>(.*)</TEXT>', re.DOTALL)
 	with open("topdocs/train/{}".format(docname)) as docfile:
 		#for each qid, make a string containing everything within <text> tags
@@ -122,11 +80,10 @@ for qnum in questions:
                         tempstring = " ".join(texts)
                         parsedtextlist.append(preprocess(tempstring))
                 #print parsedtextlist
-                print ngram(parsedtextlist, 10)
+                print ngram(parsedtextlist, 10, questions.get(qnum)[0])
 		qidtextTuple = zip(qidlist, parsedtextlist)
 		#print(qidtextTuple)
             
->>>>>>> 4ff006b4ce057419f5cf9b9f456a2ed6a55ec637
 		#retrieve appropriate 10-grams
 		#map each 10-gram to binary feature vector with question as bag of words
 		#compute vector similarity score between question & each feature vector
